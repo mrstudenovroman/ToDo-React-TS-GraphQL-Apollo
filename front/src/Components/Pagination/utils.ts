8a850b0357ff908bed055d0cd9816b166bd1cc63
Ivan Baranov <293344asd@gmail.com>
2019-11-04 18:09:47 +0600

    EL-15: поправил пагинацию

--- front/src/Components/Pagination/utils.ts
+++ front/src/Components/Pagination/utils.ts
@@ -8,8 +8,8 @@ export const pageResolver = (
   pages: number[],
   currentPage: number,
 ): { pagesWithinRange: number[]; leftDots: boolean; rightDots: boolean } => {
-  const pagesWithinRange = pages.slice(currentPage < 3 ? 0 : currentPage - 3, currentPage <= 5 ? 7 : currentPage + 2);
+  const pagesWithinRange = pages.slice(currentPage < 3 ? 0 : currentPage - 3, currentPage < 5 ? 6 : currentPage + 2);
   const leftDots = currentPage > 4 && currentPage - 3 > 0;
-  const rightDots = pages.length - (currentPage + 3) > 0;
+  const rightDots = pages.length - 3 > currentPage;
   return { pagesWithinRange, leftDots, rightDots };
 };
